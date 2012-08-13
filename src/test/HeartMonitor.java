0c382e4e8468785c6617872ecada964d63423eca
Kenneth Evans Jr <ken@kenevans.net>
2012-08-13 23:53:11 +0000

    Cleaned up warnings.
    git-svn-id: http://svn.kenevans.net/Sandbox/trunk@383 4b487630-4e8c-464e-b16a-428ddfb157b8

--- src/test/HeartMonitor.java
+++ src/test/HeartMonitor.java
@@ -1,17 +1,11 @@
 package test;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /*ftest
  * Created on Nov 12, 2011
@@ -21,7 +15,7 @@ import java.util.regex.Pattern;
 public class HeartMonitor
 {
     /** Flag to indicate if the database should be deleted first. */
-    private static final boolean deleteDatabase = true;
+    // private static final boolean deleteDatabase = true;
     /** The static format string to use for formatting dates. */
     // public static final String longFormat = "MMM dd, yyyy HH:mm:ss Z";
     public static final String longFormat = "hh:mm a MMM dd, yyyy";
@@ -33,120 +27,120 @@ public class HeartMonitor
     private static final String databaseName = "C:/Scratch/Heart Monitor/Heart Monitor 2012-12-08/HeartMonitor.db";
     private static final String databaseURL = "jdbc:sqlite:" + databaseName;
     private static final String driverClass = "org.sqlite.JDBC";
+
     // private static final String driverClass = "SQLite.JDBCDriver";
-    private static String[] names = {"Eli", "Inigo", "Dylan", "Susan"};
 
-    /**
-     * Create the database.
-     * 
-     * @return
-     */
-    // @SuppressWarnings("deprecation")
-    private static boolean createDatabase() {
-        // Connection conn = null;
-        // try {
-        // conn = DriverManager.getConnection(databaseURL);
-        // // java.lang.reflect.Method m = conn.getClass().getMethod(
-        // // "getSQLiteDatabase", null);
-        // // db = (SQLite.Database)m.invoke(conn, null);
-        // Statement tableStatement = conn.createStatement();
-        // tableStatement.execute("CREATE TABLE bob(id INTEGER, name TEXT)");
-        // tableStatement
-        // .execute("CREATE TABLE tower(tower_id INTEGER, latitude TEXT, longitude TEXT)");
-        // tableStatement
-        // .execute("CREATE TABLE usage(usage_id INTEGER, tower_id INTEGER, timestamp TEXT)");
-        //
-        // int id = 0;
-        // PreparedStatement statement = conn
-        // .prepareStatement("INSERT INTO bob VALUES(?, ?)");
-        // for(String name : names) {
-        // statement.setInt(1, id++);
-        // statement.setString(2, name);
-        // statement.execute();
-        // }
-        //
-        // // Create the faked tower data.
-        // statement = conn
-        // .prepareStatement("INSERT INTO tower VALUES(?, ?, ?)");
-        //
-        // statement.setInt(1, 1);
-        // statement.setString(2, "45.928237");
-        // statement.setString(3, "-89.696066");
-        // statement.execute();
-        //
-        // statement.setInt(1, 2);
-        // statement.setString(2, "46.000000");
-        // statement.setString(3, "-89.000000");
-        // statement.execute();
-        //
-        // // Create the faked tower usage.
-        // statement = conn
-        // .prepareStatement("INSERT INTO usage VALUES(?, ?, ?)");
-        //
-        // int usage = 0;
-        // for(int tower = 0; tower < 7; tower++) {
-        // statement.setInt(1, usage++);
-        // statement.setInt(2, (usage % 3) + 1);
-        // Calendar calendar = Calendar.getInstance();
-        // calendar.set(2011, tower, tower);
-        // statement.setString(3, calendar.getTime().toString());
-        // statement.execute();
-        // }
-        // } catch(Exception ex) {
-        // ex.printStackTrace();
-        // return false;
-        // } finally {
-        // try {
-        // if(conn != null) {
-        // conn.close();
-        // }
-        // } catch(Exception ex) {
-        // ex.printStackTrace();
-        // return false;
-        // }
-        // }
-        return true;
-    }
+    // /**
+    // * Create the database.
+    // *
+    // * @return
+    // */
+    // // @SuppressWarnings("deprecation")
+    // private static boolean createDatabase() {
+    // Connection conn = null;
+    // try {
+    // conn = DriverManager.getConnection(databaseURL);
+    // // java.lang.reflect.Method m = conn.getClass().getMethod(
+    // // "getSQLiteDatabase", null);
+    // // db = (SQLite.Database)m.invoke(conn, null);
+    // Statement tableStatement = conn.createStatement();
+    // tableStatement.execute("CREATE TABLE bob(id INTEGER, name TEXT)");
+    // tableStatement
+    // .execute("CREATE TABLE tower(tower_id INTEGER, latitude TEXT, longitude TEXT)");
+    // tableStatement
+    // .execute("CREATE TABLE usage(usage_id INTEGER, tower_id INTEGER, timestamp TEXT)");
+    //
+    // int id = 0;
+    // PreparedStatement statement = conn
+    // .prepareStatement("INSERT INTO bob VALUES(?, ?)");
+    // for(String name : names) {
+    // statement.setInt(1, id++);
+    // statement.setString(2, name);
+    // statement.execute();
+    // }
+    //
+    // // Create the faked tower data.
+    // statement = conn
+    // .prepareStatement("INSERT INTO tower VALUES(?, ?, ?)");
+    //
+    // statement.setInt(1, 1);
+    // statement.setString(2, "45.928237");
+    // statement.setString(3, "-89.696066");
+    // statement.execute();
+    //
+    // statement.setInt(1, 2);
+    // statement.setString(2, "46.000000");
+    // statement.setString(3, "-89.000000");
+    // statement.execute();
+    //
+    // // Create the faked tower usage.
+    // statement = conn
+    // .prepareStatement("INSERT INTO usage VALUES(?, ?, ?)");
+    //
+    // int usage = 0;
+    // for(int tower = 0; tower < 7; tower++) {
+    // statement.setInt(1, usage++);
+    // statement.setInt(2, (usage % 3) + 1);
+    // Calendar calendar = Calendar.getInstance();
+    // calendar.set(2011, tower, tower);
+    // statement.setString(3, calendar.getTime().toString());
+    // statement.execute();
+    // }
+    // } catch(Exception ex) {
+    // ex.printStackTrace();
+    // return false;
+    // } finally {
+    // try {
+    // if(conn != null) {
+    // conn.close();
+    // }
+    // } catch(Exception ex) {
+    // ex.printStackTrace();
+    // return false;
+    // }
+    // }
+    // return true;
+    // }
 
-    /**
-     * Deletes the database from the file system.
-     */
-    private static boolean deleteDatabase() {
-        // File file = new File(databaseName);
-        // if(file.exists()) {
-        // if(!file.canWrite()) {
-        // System.out.println("File cannot be written " + databaseName);
-        // return false;
-        // }
-        // boolean res = file.delete();
-        // if(res) {
-        // System.out.println("Delete succeeded for " + databaseName);
-        // } else {
-        // System.out.println("Delete failed for " + databaseName);
-        // System.out.println("    Check if it is in use");
-        // return false;
-        // }
-        //
-        // // try {
-        // // Thread.sleep(60000);
-        // // } catch(InterruptedException ex) {
-        // // System.out.println("Sleep was interrupted");
-        // // }
-        //
-        // // Check if it really worked
-        // file = new File(databaseName);
-        // if(file.exists()) {
-        // System.out.println("Delete actually did not work.  "
-        // + "Check if it is open somewhere.");
-        // return false;
-        // }
-        // } else {
-        // System.out
-        // .println("File does not exist so does not need to be deleted"
-        // + databaseName);
-        // }
-        return true;
-    }
+    // /**
+    // * Deletes the database from the file system.
+    // */
+    // private static boolean deleteDatabase() {
+    // File file = new File(databaseName);
+    // if(file.exists()) {
+    // if(!file.canWrite()) {
+    // System.out.println("File cannot be written " + databaseName);
+    // return false;
+    // }
+    // boolean res = file.delete();
+    // if(res) {
+    // System.out.println("Delete succeeded for " + databaseName);
+    // } else {
+    // System.out.println("Delete failed for " + databaseName);
+    // System.out.println("    Check if it is in use");
+    // return false;
+    // }
+    //
+    // // try {
+    // // Thread.sleep(60000);
+    // // } catch(InterruptedException ex) {
+    // // System.out.println("Sleep was interrupted");
+    // // }
+    //
+    // // Check if it really worked
+    // file = new File(databaseName);
+    // if(file.exists()) {
+    // System.out.println("Delete actually did not work.  "
+    // + "Check if it is open somewhere.");
+    // return false;
+    // }
+    // } else {
+    // System.out
+    // .println("File does not exist so does not need to be deleted"
+    // + databaseName);
+    // }
+    // return true;
+    // }
 
     /**
      * Prints the names of the tables in the database.
